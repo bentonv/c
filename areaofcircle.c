b4ffc2979952f9c2c4f0255e8af9ec2e92d39377
benton <bjvalenzuela@gmail.com>
2018-12-02 18:33:41 -0800

    added process function

--- areaofcircle.c
+++ areaofcircle.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
+#define PI 3.14159
+
+float process (float radius)  /*function prototype to move out the main process into a new function */
+
 main()
 {
 	float radius, area;
 	printf("Radius = ? ");
 	scanf("%f", &radius);
-	area = 3.1459 * radius * radius;
+	area = process(radius);
 	printf("Area = %f", area);
 }
+
+/*accepts a floating point as an argument
+* float a will be where the final result will be held
+* constant PI multiplied by r squared
+*/
+float process(float r)	
+{
+	float a;
+	a = PI * r * r;
+	return(a);
+}
+
